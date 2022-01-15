2c5d41f05cccb5186a77790308da62d20197f46e
sunrabbit123 <qudwls185@naver.com>
2022-01-15 15:42:25 +0900

    add : default setting

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [".env"],
+    }),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      database: "record",
+      autoLoadEntities: true,
+      synchronize: false,
+      entities: [__dirname + "/**/*/entity/*{.ts,.js}"],
+      logging: true,
+      url: process.env.DB_URL,
+    }),
+    ,
+  ],
+})
+export class AppModule {}
